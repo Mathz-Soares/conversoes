4ed0fdc7be328e4acb13541c4a19228d2f8abeb8
Mathz-Soares <matheussou487@gmail.com>
2024-01-08 12:23:30 -0300

    conversoes

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// conversão para int
+
+double numero1 = Convert.ToInt16(Console.ReadLine());
+Console.Write(numero1 + "\n");
+
+Console.Write("********************************************** \n");
+// conversão para double
+
+double numero2 = Convert.ToDouble(Console.ReadLine());
+Console.Write(numero2 + "\n");
+
+Console.Write("********************************************** \n");
+// conversão para string
+
+string Key = Console.ReadLine().ToLower();
+Console.Write(Key);
